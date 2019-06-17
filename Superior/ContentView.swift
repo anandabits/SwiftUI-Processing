8fb6ec7d8eb51ee52aa8e53a039c7c62990ae738
Matthew Johnson <matthew@anandabits.com>
2019-06-16 22:35:00 -0500

    Initial Commit

---
+++ Superior/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Superior
+//
+//  Created by Matthew Johnson on 6/16/19.
+//  Copyright © 2019 Anandabits LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
